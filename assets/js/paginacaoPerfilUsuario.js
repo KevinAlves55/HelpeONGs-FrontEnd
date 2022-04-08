c704fd63f0450717eb1b942cc5579af5c99fa1ea
Carlos <carlosgabriec@gmail.com>
2022-04-08 16:36:47 -0300

    faltando a logica para troca de conteudo

--- assets/js/paginacaoPerfilUsuario.js
+++ assets/js/paginacaoPerfilUsuario.js
@@ -1,1 +1,19 @@
 'use strict'
+
+document.getElementById("item1").addEventListener("click", ()=>{
+    document.getElementById("container-conteudo-sobre").classList.add("aparecer")
+    document.getElementById("container-conteudo-detalhes-de-conta").classList.remove("aparecer")
+    document.getElementById("container-conteudo-detalhes-do-endereco").classList.remove("aparecer")
+})
+
+document.getElementById("item2").addEventListener("click", ()=>{
+    document.getElementById("container-conteudo-sobre").classList.remove("aparecer")
+    document.getElementById("container-conteudo-detalhes-de-conta").classList.add("aparecer")
+    document.getElementById("container-conteudo-detalhes-do-endereco").classList.remove("aparecer")
+})
+
+document.getElementById("item3").addEventListener("click", ()=>{
+    document.getElementById("container-conteudo-sobre").classList.remove("aparecer")
+    document.getElementById("container-conteudo-detalhes-de-conta").classList.remove("aparecer")
+    document.getElementById("container-conteudo-detalhes-do-endereco").classList.add("aparecer")
+})
