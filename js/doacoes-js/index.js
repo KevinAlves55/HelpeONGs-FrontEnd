fdadcbd5dfa60f3335afbda84991863c104a30c3
Kevin <programador.kevin4773@gmail.com>
2022-04-18 16:55:03 -0300

    Pegando todas as ONGs do Back-End

--- js/doacoes-js/index.js
+++ js/doacoes-js/index.js
@@ -4,23 +4,6 @@ import ApiRequest from "../utils/ApiRequest.js";
 import { openModal, closeModal } from "./modal.js";
 import { openFiltro, filtrar } from "./filtro.js";
 
-const CriarRecomendados = ({id, nome, foto}) => {
-
-    const corpo = document.createElement("div");
-
-    corpo.innerHTML =
-    `<div class="ongs-opcoes">
-        <div>
-            <img src="${foto}" alt="Ongs perfil" title="Foto Ong">
-            <h2>${nome}</h2>
-        </div>
-        <button data-idRecomendados="${id}">DOAR</button>
-    </div>`;
-
-    return corpo;
-
-}
-
 const CarregarRecomendados = async () => {
 
     const container = document.getElementById("recomendados-ongs");
@@ -32,6 +15,51 @@ const CarregarRecomendados = async () => {
 
 }
 
+const CriarRecomendados = ({id, nome, foto}) => {
+
+    const corpo = document.createElement("div");
+
+    corpo.innerHTML =
+    `<div class="ongs-opcoes">
+        <div>
+            <img src="${foto}" alt="Ongs perfil" title="Foto Ong">
+            <h2>${nome}</h2>
+        </div>
+        <button type="button" data-idRecomendados="${id}">DOAR</button>
+    </div>`;
+
+    return corpo;
+
+}
+
+const CarregarTodasONGs = async () => {
+
+    const container = document.getElementById("ongs");
+    const objeto = await ApiRequest("GET", "http://localhost:3131/ong/all");
+    const corpo = objeto.data;
+    const cards = corpo.map(CriarONGs);
+    container.replaceChildren(...cards);
+
+}
+
+const CriarONGs = ({id, nome, numeroDeSeguidores, foto}) => {
+
+    const corpo = document.createElement("div");
+
+    corpo.innerHTML =
+    `<div class="ongs-card">
+        <img src="assets/img/favoritar-sem-preenchimento.png" class="img-coracao" alt="Favoritos" title="Icon Coração">
+        <img src="${foto}" alt="${nome}" title="Imagem da ONG" class="img-ong">
+        <h2>${nome}</h2>
+        <span>${numeroDeSeguidores} seguidores</span>
+        <button type="button" data-idONGs="${id}">DOAR</button>
+    </div>`;
+
+    return corpo;
+
+}
+
 CarregarRecomendados();
+CarregarTodasONGs();
 document.getElementById("botao-filtro").addEventListener("click", openFiltro);
 document.getElementById("limpar-campos").addEventListener("click", filtrar);
