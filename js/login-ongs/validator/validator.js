52f46c4615197477439b1bb5f33388287891947b
Kevin <programador.kevin4773@gmail.com>
2022-04-18 14:41:42 -0300

    Fazendo a logica do login

---
+++ js/login-ongs/validator/validator.js
@@ -0,0 +1,34 @@
+const button = document.getElementById('btn-login')
+
+button.addEventListener('click', (event) => {
+    event.preventDefault();
+alert('oi')
+
+    const email = document.getElementById('email')
+    const senha = document.getElementById('senha')
+
+    if (email.value == ''){
+        email.classList.add("errorInput")
+    }   else{
+        email.classList.remove("errorInput")
+    }
+
+    if (senha.value == ''){  
+        senha.classList.add("errorInput")   
+    }   else{
+        senha.classList.remove("errorInput")
+    }
+
+    if (email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1 || email.value.indexOf(".") - email.value.indexOf("@") == 1){
+        email.classList.add("errorInput")
+    }   else{
+        email.classList.remove("errorInput")
+    }
+
+    if (senha.value.length <= 5){
+        senha.classList.add("errorInput")
+    }   else{
+        senha.classList.remove("errorInput")
+    }
+
+})
