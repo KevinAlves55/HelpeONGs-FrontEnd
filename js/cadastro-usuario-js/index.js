a04778c4df72dcd674ce27994a6f347518874928
carlosgabriel1234 <carlosgabriec@gmail.com>
2022-04-20 16:55:09 -0300

    Finalizando a integração

--- js/cadastro-usuario-js/index.js
+++ js/cadastro-usuario-js/index.js
@@ -1,31 +1,44 @@
 "use strict";
 
 import ApiRequest from "../utils/ApiRequest.js";
-document.getElementById("formButton").addEventListener("click", () => {
+
+const redirect = () => {
+    window.location.href = "login.html";
+};
+
+const cadastrarUsuario = async () => {
+    
     const dom = {
         nome: document.getElementById("nome").value,
         email: document.getElementById("email").value,
         password: document.getElementById("senha").value,
         confirmPassword: document.getElementById("confirmarSenha").value,
     };
-    
-    if (dom.password !== dom.confirmPassword) {
+
+    if (dom.nome === "" || dom.email === "" || dom.password === "" || dom.confirmPassword === "") {
+        alert("Preencha todos os campos");
+        return;
+    } else if (dom.password !== dom.confirmPassword) {
         alert("Senhas não conferem!");
         return;
     }
 
-    
-
-    const ongData = {
-        nome: dom.name,
+    const userData = {
+        nome: dom.nome,
         email: dom.email,
         senha: dom.password,
     };
 
-    
-
     let request;
-    console.log("Waiting request 0s: ", request);
-    request = ApiRequest("POST", "http://localhost:3131/ong/pre-register", ongData);
-    console.log("Waiting request 1s: ", request);
-});
+    request = await ApiRequest("POST", "http://localhost:3131/user/pre-register", userData);
+    console.log(request);
+
+    if (request.status === 200) {
+        alert("Cadastrado com sucesso");
+        redirect();
+    } else if (request.status === 400) {
+        alert(request.message);
+    }
+};
+
+document.getElementById("formButton").addEventListener("click", cadastrarUsuario);
