736dcae9287e5a3dd6c33f99ab5d2ee50078f8b0
carlosgabriel1234 <carlosgabriec@gmail.com>
2022-04-26 16:55:18 -0300

    tela estetia finalizada

---
+++ js/endereco.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const limparFormulario = (rua) =>{
+    document.getElementById('rua').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
+}
+
+
+const preencherFormulario = (rua) =>{
+    document.getElementById('rua').value = rua.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+
+const cepValido = (cep) => cep.length == 8 && eNumero(cep); 
+
+const pesquisarCep = async() => {
+    limparFormulario();
+    
+    const cep = document.getElementById('cep').value.replace("-","");
+    console.log(cep);
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    if (cepValido(cep)){
+        const dados = await fetch(url);
+        const endereco = await dados.json();
+        if (endereco.hasOwnProperty('erro')){
+            document.getElementById('endereco').value = 'CEP não encontrado!';
+        }else {
+            preencherFormulario(endereco);
+        }
+    }else{
+        document.getElementById('endereco').value = 'CEP incorreto!';
+    }
+     
+}
+
+document.getElementById('cep')
+        .addEventListener('focusout',pesquisarCep);
