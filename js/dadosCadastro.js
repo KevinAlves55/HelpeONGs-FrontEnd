d63d9e6741cfc90a8eb00aa01bc1f30035c30764
carlosgabriel1234 <carlosgabriec@gmail.com>
2022-04-27 16:58:14 -0300

    recebendo dados localStorage

--- js/dadosCadastro.js
+++ js/dadosCadastro.js
@@ -1,2 +1,25 @@
 'use strict'
 
+document.getElementById('name').value = localStorage.nome;
+document.getElementById('email').value = localStorage.email;
+document.getElementById('data').value = localStorage.dataNascimento;
+document.getElementById('celular').value = localStorage.celular;
+document.getElementById('telefone').value = localStorage.telefone;
+
+
+var salvarDados = function(){
+
+    var nome = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var dataNascimento = document.getElementById('data').value;
+    var celular = document.getElementById('celular').value;
+    var telefone = document.getElementById('telefone').value;
+
+    localStorage.setItem('nome', nome);
+    localStorage.setItem('email', email);
+    localStorage.setItem('dataNascimento', dataNascimento);
+    localStorage.setItem('celular', celular);
+    localStorage.setItem('telefone', telefone);
+}
+
+document.onchange = salvarDados;
