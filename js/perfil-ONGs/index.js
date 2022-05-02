dd77d42153b00c3ef1bac9cefcd82bf995ee818d
Kevin <programador.kevin4773@gmail.com>
2022-05-02 15:45:47 -0300

    Finalizando as validações

--- js/perfil-ONGs/index.js
+++ js/perfil-ONGs/index.js
@@ -1,6 +1,11 @@
 'use strict';
 
-import { checkInputs, errorValidation } from "../validator/validatorPerfilONG.js";
+import { 
+checkInputsDetalhesConta, 
+checkInputsDetalhesOng,
+checkInputsDetalhesEndereco,
+errorValidation 
+} from "../validator/validatorPerfilONG.js";
 
 // Objeto de captura das INPUTS
 const nome = document.getElementById('nomeOng');
@@ -13,13 +18,13 @@ const qtdaMembros = document.getElementById("qtdaMembrosOng");
 const dataFundacao = document.getElementById("fundacaoOng");
 const historia = document.getElementById("historiaOng");
 const cep = document.getElementById("cep");
-const estado = document.getElementById('estado');
-const cidade = document.getElementById('cidade');
-const bairro = document.getElementById('bairro');
-const endereco = document.getElementById('endereco');
-const numero = document.getElementById('numero');
-const complemento = document.getElementById('complemento');
-const linkSite = document.getElementById('linkSiteOng');
+const estado = document.getElementById("estado");
+const cidade = document.getElementById("cidade");
+const bairro = document.getElementById("bairro");
+const endereco = document.getElementById("endereco");
+const numero = document.getElementById("numero");
+const complemento = document.getElementById("complemento");
+const linkSite = document.getElementById("linkSiteOng");
 const pix = document.getElementById("pixOng");
 const agencia = document.getElementById("agenciaOng");
 const conta = document.getElementById("contaOng");
@@ -29,18 +34,20 @@ const sitePatrocinador = document.getElementById("linkSitePatrocinador");
 
 function dadosDetalhesConta() {
 
-    const validacoes = checkInputs();
+    const validacoesDadosDetalhesConta = checkInputsDetalhesConta();
 
     let result;
-    validacoes.map(status => {
+    validacoesDadosDetalhesConta.map(status => {
         status === false ? result = false : "";
     });
     
     if (result != false) {
+
         const dadosDetalhes = {
             nomeData: nome.value,
             cnpjData: cnpj.value
         }
+        console.log(dadosDetalhes);
         localStorage.setItem("detalhesConta", JSON.stringify(dadosDetalhes));
     
         const dadosDetalhesContatos = {
@@ -49,6 +56,9 @@ function dadosDetalhesConta() {
             telefoneData: telefone.value
         }
         localStorage.setItem("detalhesContatos", JSON.stringify(dadosDetalhesContatos));
+
+    } else {
+        console.log("erro nas validações");
     }
 }
 document.getElementById("formButton").addEventListener("click", dadosDetalhesConta);
@@ -56,60 +66,102 @@ document.getElementById("formButton").addEventListener("click", dadosDetalhesCon
 
 function dadosDetalhesONG() {
 
-    const DadosdetalhesONGs = {
-        descricaoData: descriacao.value,
-        qtdaMembrosData: qtdaMembros.value,
-        dataFundacaoData: dataFundacao.value,
-        historiaData: historia.value
+    const validacoesDadosDetalhesONG = checkInputsDetalhesOng();
+
+    let result;
+    validacoesDadosDetalhesONG.map(status => {
+        status === false ? result = false : "";
+    });
+    
+    if (result != false) {
+        const DadosdetalhesONGs = {
+            descricaoData: descriacao.value,
+            qtdaMembrosData: qtdaMembros.value,
+            dataFundacaoData: dataFundacao.value,
+            historiaData: historia.value
+        }
+        localStorage.setItem("detalhesONGs", JSON.stringify(DadosdetalhesONGs));
     }
-    localStorage.setItem("detalhesONGs", JSON.stringify(DadosdetalhesONGs));
+
 }
 document.getElementById("button-detalhes-ONG").addEventListener("click", dadosDetalhesONG);
 
-
 function dadosDetalhesEndereco() {
 
-    const dadosDetalhesEndereco = {
-        cepData: cep.value,
-        estadoData: estado.value,
-        cidadeData: cidade.value,
-        bairroData: bairro.value,
-        ruaData: endereco.value,
-        numeroData: numero.value,
-        complementoData: complemento.value
-    }
-    localStorage.setItem("detalhesEndereco", JSON.stringify(dadosDetalhesEndereco)); 
-}
+    const validacoesDadosDetalhesEndereco = checkInputsDetalhesEndereco();
 
+    console.log(validacoesDadosDetalhesEndereco);
+
+    let result;
+    validacoesDadosDetalhesEndereco.map(status => {
+        status === false ? result = false : "";
+    });
+    
+    if (result != false) {
+
+        const dadosDetalhesEndereco = {
+            cepData: cep.value,
+            estadoData: estado.value,
+            cidadeData: cidade.value,
+            bairroData: bairro.value,
+            ruaData: endereco.value,
+            numeroData: numero.value,
+            complementoData: complemento.value
+        }
+        localStorage.setItem("detalhesEndereco", JSON.stringify(dadosDetalhesEndereco));
+
+    } else {
+        console.log("error");
+    }
+}
 document.getElementById("button-detalhes-endereco").addEventListener("click", dadosDetalhesEndereco);
 
 
 function dadosMeiosDoacoes() {
 
-    const dadosMeiosDoacoes = {
-        siteData: linkSite.value,
-        pixData: pix.value
-    }
-    localStorage.setItem("MeiosDoacoes", JSON.stringify(dadosMeiosDoacoes)); 
+    const validacoesDadosMeiosDoacoes = checkInputsDadosMeiosDoacoes();
 
-    const dadosBancarios = {
-        agenciaData: agencia.value,
-        contaData: conta.value,
-        tipoContaData: tipoConta.value
+    let result;
+    validacoesDadosMeiosDoacoes.map(status => {
+        status === false ? result = false : "";
+    });
+    
+    if (result != false) {
+
+        const dadosMeiosDoacoes = {
+            siteData: linkSite.value,
+            pixData: pix.value
+        }
+        localStorage.setItem("MeiosDoacoes", JSON.stringify(dadosMeiosDoacoes)); 
+    
+        const dadosBancarios = {
+            agenciaData: agencia.value,
+            contaData: conta.value,
+            tipoContaData: tipoConta.value
+        }
+        localStorage.setItem("Bancario", JSON.stringify(dadosBancarios)); 
+
     }
-    localStorage.setItem("Bancario", JSON.stringify(dadosBancarios)); 
 }
-
 document.getElementById("button-meiosDoacoes").addEventListener("click", dadosMeiosDoacoes);
 
 
-function dadosPatocinios() {
+// function dadosPatocinios() {
 
-    const dadosPatrocinios = {
-        patrocinadorData: nomePatrocinador.value,
-        sitePatrocinadorData: sitePatrocinador.value
-    }
-    localStorage.setItem("Patrocinios", JSON.stringify(dadosPatrocinios)); 
+//     let result;
+//     validacoes.map(status => {
+//         status === false ? result = false : "";
+//     });
+    
+//     if (result != false) {
 
-}
-document.getElementById("button-patrocinios").addEventListener("click", dadosPatocinios);
+//         const dadosPatrocinios = {
+//             patrocinadorData: nomePatrocinador.value,
+//             sitePatrocinadorData: sitePatrocinador.value
+//         }
+//         localStorage.setItem("Patrocinios", JSON.stringify(dadosPatrocinios)); 
+
+//     }
+
+// }
+// document.getElementById("button-patrocinios").addEventListener("click", dadosPatocinios);
