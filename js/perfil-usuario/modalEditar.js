65ba3790d80164d4cf916fee50490f1e663d8b81
carlosgabriel1234 <carlosgabriec@gmail.com>
2022-05-12 18:48:58 -0300

    ajustando reparos estilizacao

--- js/perfil-usuario/modalEditar.js
+++ js/perfil-usuario/modalEditar.js
@@ -1,2 +1,14 @@
 "use strict";
 
+const btn = document.querySelector('.btnModal');
+const modal = document.querySelector('.modalEditar');
+const modalSair = document.querySelector('.botaoSairEditar');
+
+
+btn.addEventListener('click', function () {
+    modal.classList.add('bg-active');
+})
+
+modalSair.addEventListener('click', function () {
+    modal.classList.remove('bg-active');
+})
